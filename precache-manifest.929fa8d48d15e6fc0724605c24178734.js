b3c69f470296cc4093ab6a6cfd5d26cd3699491c
CK <ck@CKs-MacBook-Pro.local>
2019-01-12 18:28:26 +0900

    Updates

---
+++ precache-manifest.929fa8d48d15e6fc0724605c24178734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "7a1db57cb36751a8e1bc",
+    "url": "/movie_app/static/js/main.7a1db57c.chunk.js"
+  },
+  {
+    "revision": "ad67683f6e90501a5d47",
+    "url": "/movie_app/static/js/1.ad67683f.chunk.js"
+  },
+  {
+    "revision": "7a1db57cb36751a8e1bc",
+    "url": "/movie_app/static/css/main.8713b9a6.chunk.css"
+  },
+  {
+    "revision": "ca3a5d1e672af99ad80ba7067c8ebc44",
+    "url": "/movie_app/index.html"
+  }
+];
